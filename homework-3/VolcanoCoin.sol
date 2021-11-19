84481ec33ca2674473ec42a6b0e84ec06c1f6e2e
JoshuaLelon <jlelonmitchell@gmail.com>
2021-11-19 14:41:42 -0600

    final copy hw 3

--- homework-3/VolcanoCoin.sol
+++ homework-3/VolcanoCoin.sol
@@ -1,26 +1,3 @@
-// Additional Guidance:
-// Please use the Score contract tutorial as guidance, it has plenty of tips for building your ERC20 contract.
-// At each point where you change your contract you should re deploy to the JavascriptVM and test your changes.
-// 1. In Remix, create a new file called VolcanoCoin.sol .
-// 2. Define the pragma compiler version to ^0.8.0 .
-// 3. Before the pragma version, add a license identifer // SPDX-License-Identifier: UNLICENSED .
-// 4. Create a contract called VolcanoCoin.
-// 5. Create a variable to hold the total supply of 10000.
-// 6. Make a public function that returns the total supply.
-// 7. Make a function that can increase the total supply. Inside the function,
-//    add 1000 tokens to the current total supply.
-// 8. We probably want users to be aware of functions in the contract for greater transparency,
-//    but to make them all public will create some security risks (e.g. we don’t want users to be able to change the total supply).
-//    Initialise an address variable called owner .
-// 9. Next, create a modifier which only allows an owner to execute certain functions.
-// 10. Make your increase total supply function public , but add your modifier so that only the owner
-//     can execute it.
-// 11. The contract owner’s address should only be updateable in one place. Create a constructor and
-//     within the constructor, store the owner’s address.
-
-// 12. It would be useful to broadcast a change in the total supply. Create an event that emits the
-//     new value whenever the total supply changes. When the supply changes, emit this event.
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
@@ -28,10 +5,10 @@ contract VolcanoCoin {
     uint256 totalSupply = 10000;
     address owner;
 
-    event SupplyIncrease();
+    event SupplyIncrease(uint256 newSupply);
 
-    modifier requireOwnership() {
-        require(transaction.owner == owner);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
         _;
     }
 
@@ -43,8 +20,8 @@ contract VolcanoCoin {
         return totalSupply;
     }
 
-    function increaseSupply() public requireOwnership {
+    function increaseSupply() public onlyOwner {
         totalSupply += 1000;
-        emit SupplyIncrease;
+        emit SupplyIncrease(totalSupply);
     }
 }
