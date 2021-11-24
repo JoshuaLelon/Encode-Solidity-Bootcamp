5b71e31a18e659c4a11aa45ee0429c23d9d6ba42
JoshuaLelon <jlelonmitchell@gmail.com>
2021-11-23 22:21:19 -0600

    did a few steps of work

--- homework-6/contracts/VolcanoToken.sol
+++ homework-6/contracts/VolcanoToken.sol
@@ -4,4 +4,28 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VolcanoToken is ERC721("VolcanoToken", "VCTK"), Ownable {}
+contract VolcanoToken is ERC721("VolcanoToken", "VCTK"), Ownable {
+    struct Metadata {
+        uint256 timestamp;
+        uint256 tokenId;
+        string tokenURI;
+    }
+
+    mapping(address => Metadata[]) ownership;
+
+    uint256 tokenId;
+
+    function mint() {
+        Metadata memory newTokenData = Metadata(
+            block.timestamp,
+            tokenId,
+            "any string"
+        );
+        ownership[msg.sender].push(newTokenData);
+        tokenId++;
+    }
+
+    function burn(uint256 tokenId) {
+        
+    }
+}
